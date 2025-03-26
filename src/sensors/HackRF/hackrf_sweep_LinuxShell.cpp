bac12b63d01a680910ebbc96e101560034f4a7e7
dth0024 <dth0024@uah.edu>
2025-03-26 10:15:49 -0500

    Cleared TODOs, added comments, and cleaned up code.  This should work fine!

--- src/sensors/HackRF/hackrf_sweep_LinuxShell.cpp
+++ src/sensors/HackRF/hackrf_sweep_LinuxShell.cpp
@@ -1,3 +1,9 @@
+//hackrf_sweep_LinuxShell.cpp
+//Made by Dawson Hampton, dth0024@uah.edu
+//2025-3-26
+
+//This program demonstrates the sweep() function stored in hsLS.cpp.
+
 #include <iostream>
 #include <string>
 #include <stdlib.h>
@@ -11,8 +17,15 @@ int main() {
 	cout << "Starting...  Press Ctrl-C to exit!" << endl;
 
     //Create the command strings to be executed.
-    //TODO: Explain what these commands mean.
 
+    //These commands run the Linux Shell's hackrf_sweep command.
+    //The flags are described as:
+        //-f determines the frequency range (in MHz) to sweep non-inclusive.  -f 5:10 starts at 5 MHz and ends before 10 MHz.
+        //-N determines the number of sweeps to do before the command ends.  Because we constantly call the command, leave -N 1.
+        //-w is the frequency bin width, or "frequency resolution", in Hz.  That is essentially how big of steps it takes while sweeping.  The default is 1 MHz.
+        //-l is the LVA gain in dB.  The default LVA gain in SDR++ is 40 dB, so I suggest 40 dB as well.
+        //-g is the GVA gain in dB.  I read online that a recommended value is 8 dB, so I made it that here.
+    //2>/dev/null is not a flag.  The hackrf_sweep command has debug output that spams the command line and 2>/dev/null suppresses it.
     string nineMHzCommand = "hackrf_sweep -f 902:928 -N 1 -w 1000000 -l 40 -g 8 2>/dev/null";
     string twoGHzCommand = "hackrf_sweep -f 2400:2495 -N 1 -w 1000000 -l 40 -g 8 2>/dev/null";
     string fiveGHzCommand = "hackrf_sweep -f 5150:5895 -N 1 -w 1000000 -l 40 -g 8 2>/dev/null";
@@ -23,8 +36,6 @@ int main() {
     vector<tuple<uint64_t, int>> nineMHz_data;
     vector<tuple<uint64_t, int>> twoGHz_data;
     vector<tuple<uint64_t, int>> fiveGHz_data;
-
-    //TODO: Add comments.  Lots and lots of comments.
 
     while (true) {
         cout << "Run!" << endl;
