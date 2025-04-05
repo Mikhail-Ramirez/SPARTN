ceda76a9d3d646066cb41ef576232a08f4c96fbf
km0159 <km0159@uah.edu>
2025-04-05 19:58:18 +0000

    dot showing

--- src/main.py
+++ src/main.py
@@ -67,11 +67,11 @@ def main():
             # Estimate the source location via trilateration
             estimated_position, r1, r2 = localize_source(time_lags, reordered_mics)
 
-            #if estimated_position[0] is not None:
-            #    send_location(estimated_position[0], estimated_position[1])
+            if estimated_position[0] is not None:
+               send_location(estimated_position[0], estimated_position[1])
             # DEBUG FOR SENDING
-            estimated_position, r1, r2 = 0, 0, 0
-            send_location(1, 1)
+            # estimated_position, r1, r2 = 0, 0, 0
+            # send_location(1, 1)
 
             # Log the measurement to file with a timestamp
             timestamp = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
