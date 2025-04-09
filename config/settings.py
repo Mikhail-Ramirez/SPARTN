43581d4cadd5f4f62b9fff9ba405bbd57177898a
tjb0045 <tjb0045@uah.edu>
2025-04-09 22:02:15 +0000

    testing 4th mic

--- config/settings.py
+++ config/settings.py
@@ -6,17 +6,17 @@ from datetime import datetime
 SAMPLE_RATE = 48000
 WINDOW_DURATION = 1.0
 CHUNK_DURATION = 0.1
-MIC_ORDER = [2, 3, 4]   # Add 4th ALSA index  
+MIC_ORDER = [2, 3, 4, 5]   # 4th ALSA index: 5 
 
 CHANNELS = 1               # Mono recording
 SPEED_OF_SOUND = 343         # m/s
 
 # Microphone positions (ALSA card numbers → Physical positions)
 MIC_POSITIONS = {
-    # 1: np.array([0, 0]), # Need to get the correct ALSA index for the 4th mic 
-    2: np.array([0, 0]),# Mic2 
-    3: np.array([0, 0]),# Mic3
-    4: np.array([0, 0]) # Mic4
+    2: np.array([0, 0]), # Mic2 
+    3: np.array([0, 0]), # Mic3
+    4: np.array([0, 0]), # Mic4
+    5: np.array([0, 0])  # Mic5     Should be the 4th mic ALSA value 
 }
 
 # CSV logging file
